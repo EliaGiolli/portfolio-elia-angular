ac7b9ca1891f28a2ec90b033171a0dd6586b2936
Elia Giolli <eliagiolli22@gmail.com>
2026-04-22 18:49:33 +0200

    added some more personal projects to the mock data

--- src/app/core/models/projects.model.ts
+++ src/app/core/models/projects.model.ts
@@ -34,7 +34,7 @@ export const projects:ProjectsTypes[] = [
         "img_path": "",
         "description": "ShelfSpot is a modern web application that allows users to explore and manage books through the OpenLibrary AP",
         "technologies": ["react", "typescript", "tailwindcss"],
-        "demo_link": "",
+        "demo_link": "https://vercel.com/elias-projects-5d10482a/shelf-spot-react",
         "tech_stack": TechStack.frontend
     },
     {
@@ -43,7 +43,7 @@ export const projects:ProjectsTypes[] = [
         "img_path": "",
         "description": "is a front-end web application that simulates a professional dashboard. The purpose is to show a dyamic work table,a presence chart and a time summary",
         "technologies": ["react", "typescript", "tailwindcss"],
-        "demo_link": "",
+        "demo_link": "https://clock-wise-react-ts.vercel.app/",
         "tech_stack": TechStack.frontend
     },
     {
@@ -52,6 +52,33 @@ export const projects:ProjectsTypes[] = [
         "img_path": "",
         "description": "A robust REST API built with Node.js, Express, TypeScript, and MongoDB that provides JWT-based authentication and comprehensive task management functionality.",
         "technologies": ["nodedotjs", "express", "typescript", "mongodb"],
+        "demo_link": "",
+        "tech_stack": TechStack.backend
+    },
+    {
+        "id":7,
+        "project_name": "Bookstore API",
+        "img_path": "",
+        "description": "A robust REST API built with Nest.js, TypeScript, and SQLite that provides JWT-based authentication and comprehensive book store management functionality.",
+        "technologies": ["nodedotjs", "nestjs", "typescript", "postgresql"],
+        "demo_link": "",
+        "tech_stack": TechStack.backend
+    },
+    {
+        "id":8,
+        "project_name": "Orizon Travel Agency",
+        "img_path": "",
+        "description": "Orizon specializes in providing exceptional travel experiences across South America and Asia, featuring a clean, premium design with smooth animations and intuitive user experience.",
+        "technologies": ["react", "typescript", "tailwindcss"],
+        "demo_link": "",
+        "tech_stack": TechStack.backend
+    },
+    {
+        "id":9,
+        "project_name": "PennyWise",
+        "img_path": "",
+        "description": "A modern expense tracking application built with React and Redux Toolkit, showcasing the evolution from local state management to enterprise-level state management.",
+        "technologies": ["react", "javascript", "tailwindcss"],
         "demo_link": "",
         "tech_stack": TechStack.backend
     },
